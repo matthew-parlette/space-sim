b9fef289969d1deaa6d56adb643b7fb5ddeb3cbd
Matt Parlette <matthew.parlette@gmail.com>
2014-05-26 19:36:37 -0400

    Added logging options to console and file

--- server.py
+++ server.py
@@ -1,6 +1,30 @@
-import sys
+import argparse
+import logging
+import os
 
 if __name__ == "__main__":
+  # Parse command line arguments
   parser = argparse.ArgumentParser(description='Process command line options.')
+  parser.add_argument('-d','--debug', action='store_true', help='Enable debug logging')
   parser.add_argument('--version', action='version', version='0')
   args = parser.parse_args()
+
+  # Setup logging options
+  log_level = logging.DEBUG if args.debug else logging.INFO
+  log = logging.getLogger(os.path.basename(__file__))
+  log.setLevel(log_level)
+  formatter = logging.Formatter('%(asctime)s:%(name)s:%(levelname)s:%(message)s')
+
+  ## Console Logging
+  ch = logging.StreamHandler()
+  ch.setLevel(log_level)
+  ch.setFormatter(formatter)
+  log.addHandler(ch)
+
+  ## File Logging
+  fh = logging.FileHandler(os.path.basename(__file__) + '.log')
+  fh.setLevel(log_level)
+  fh.setFormatter(formatter)
+  log.addHandler(fh)
+
+  log.info("Initializing")
