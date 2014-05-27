a58b7ede76cef6a5db3996646ea55a33bfea0393
Matt Parlette <matthew.parlette@gmail.com>
2014-05-26 21:52:22 -0400

    send login action

--- test_client.py
+++ test_client.py
@@ -1,11 +1,12 @@
 #!/usr/bin/env python
 
 import socket
+import json
 
 TCP_IP = '127.0.0.1'
 TCP_PORT = 10344
 BUFFER_SIZE = 1024
-MESSAGE = "test client message"
+MESSAGE = json.dumps({'action': 'login'})
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.connect((TCP_IP, TCP_PORT))
