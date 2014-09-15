b68888ac4b23c6aaa116c4d304e5afab1ae24e53
Matt Parlette <matthew.parlette@gmail.com>
2014-09-14 20:55:56 -0400

    Add game creation and adding of players.
    Various improvements to the menus, as it will now:
    * execute a Menu function if found, or
    * execute Game function otherwise
    
    Menus also stack so they should roll back to the last menu when the current menu exits. This could probably be tested.

--- game.py
+++ game.py
@@ -6,6 +6,32 @@ import os
 import datetime
 import sys
 import menu
+from uuid import uuid4
+
+class Entity(object):
+    """docstring for Entity"""
+    def __init__(self, id):
+        super(Entity, self).__init__()
+        self.id = id
+
+    def __repr__(self):
+        return str(self.id)
+
+class Sector(Entity):
+    """docstring for Sector"""
+    def __init__(self, id, warps = {}):
+        super(Sector, self).__init__(id)
+        self.warps = warps
+
+    def __repr__(self):
+        return "%s (warps to %s)" % (self.id,
+            ','.join(str(warp) for warp in self.warps))
+
+class Player(Entity):
+    """docstring for Player"""
+    def __init__(self, id):
+        super(Player, self).__init__(id)
+        self.name = id
 
 class Manager(object):
     """The superclass for all objects that manage other objects.
@@ -20,7 +46,45 @@ class Game(Manager):
     """docstring for Game"""
     def __init__(self, log = None):
         super(Game, self).__init__(log)
+        self.id = None
+        self.sectors = None
+        self.players = None
         self.log.debug("%s initialized" % self.__class__.__name__)
+
+    def new(self):
+        self.log.debug("Starting a new game...")
+        self.id = uuid4()
+        self.players = []
+        self.log.info("Executing big bang...")
+        self.big_bang()
+
+
+    def big_bang(self):
+        """Generate sectors for the universe"""
+        self.log.debug("Executing big bang on game %s" % self.id)
+        self.sectors = {
+            Sector(int(1),warps = {int(2),int(3),int(4),int(5)}),
+            Sector(int(2),warps = {int(1),int(3),int(4),int(5)}),
+            Sector(int(3),warps = {int(1),int(2),int(4),int(5)}),
+            Sector(int(4),warps = {int(1),int(2),int(3),int(5)}),
+            Sector(int(5),warps = {int(1),int(2),int(3),int(4)})
+        }
+
+    def add_player(self, player_name):
+        """Add a new player to this game"""
+        self.log.debug("Player list before add: %s" % self.players)
+        self.players.append(Player(player_name))
+        self.log.debug("Player list after add: %s" % self.players)
+
+    def start(self):
+        """Start the game"""
+        self.log.info("Starting game %s..." % self.id)
+        self.log.info("Players:\n\t%s\nSector Map:\n\t%s\n" %
+            (
+                "\n\t".join(str(p) for p in self.players),
+                "\n\t".join(str(s) for s in self.sectors)
+            )
+        )
 
     def quit(self):
         sys.exit(0)
--- menu.py
+++ menu.py
@@ -13,7 +13,7 @@ class Menu(object):
                  default = None):
         super(Menu, self).__init__()
         self.log = log
-        self.log.debug("Game %s provided on init")
+        self.log.debug("Game %s provided on init" % game.id)
         self.game = game
         self.heading = heading
         self.options = options
@@ -23,13 +23,26 @@ class Menu(object):
         self.log.debug("%s initialized" % self.__class__.__name__)
 
     def display(self):
-        print "%s\n%s" % (self.heading,'=' * len(self.heading))
-        for key,value in self.options.iteritems():
-            print "  %s: %s" % (key,value)
-        print self.prompt,
-        selection = self.getch()
-        func = getattr(self.game, self.options[selection])
-        func()
+        while True:
+            self.pre_prompt_hook()
+            print "%s\n%s" % (self.heading,'=' * len(self.heading))
+            for key,value in self.options.iteritems():
+                print "  %s: %s" % (key,value)
+            print self.prompt,
+            selection = self.getch()
+            # Is the function defined in the menu?
+            if hasattr(self,self.options[selection]):
+                func = getattr(self, self.options[selection])
+            elif hasattr(self.game,self.options[selection]):
+                func = getattr(self.game, self.options[selection])
+            else:
+                self.log.error(
+                    "Function %s was not found in menu %s or Game" % (
+                        str(self.options[selection]),
+                        str(self.__class__.__name__)
+                    )
+                )
+            func()
 
     def pre_prompt_hook(self):
         pass
@@ -37,7 +50,41 @@ class Menu(object):
 class MainMenu(Menu):
     """docstring for MainMenu"""
     def __init__(self, log, game):
-        super(MainMenu, self).__init__(log = log,
-                                       game = game,
-                                       heading = "Main Menu",
-                                       options = {'q': "quit"})
+        options = {
+            'q': "quit",
+            'n': "new"
+        }
+        super(MainMenu, self).__init__(
+            log = log,
+            game = game,
+            heading = "Main Menu",
+            options = options
+        )
+
+    def new(self):
+        NewGameMenu(self.log,self.game).display()
+
+class NewGameMenu(Menu):
+    """docstring for NewGameMenu"""
+    def __init__(self, log, game):
+        options = {
+            'q': "quit",
+            's': "start",
+            'a': "add_player"
+        }
+        game.new()
+        super(NewGameMenu, self).__init__(
+            log = log,
+            game = game,
+            heading = "New Game",
+            options = options
+        )
+
+    def add_player(self):
+        player_name = raw_input("Enter new player name: ")
+        self.game.add_player(player_name)
+
+    def start(self):
+        self.game.start()
+        self.game.quit()
+        # SectorMenu(self.log,self.game).display()
